fc978ec79b349616c1b868665750a3a864ef8aeb
杨宏琛 <yanghongchen@bupt.edu.cn>
2023-03-28 16:02:40 +0800

    first commit

---
+++ src/utils/testCases.js
@@ -0,0 +1,113 @@
+export const courseData = [
+    {
+        id: 1,
+        name: 'Calculus',
+        dayOfWeek: 'Monday',
+        startTime: '9:00',
+        duration: 2,
+        frequency: 'weekly',
+        location: 'Room 101',
+    },
+    {
+        id: 2,
+        name: 'Physics',
+        dayOfWeek: 'Tuesday',
+        startTime: '13:00',
+        duration: 3,
+        frequency: 'biweekly',
+        location: 'Room 201',
+    },
+    {
+        id: 3,
+        name: 'Computer Science',
+        dayOfWeek: 'Friday',
+        startTime: '10:00',
+        duration: 1.5,
+        frequency: 'weekly',
+        location: 'Room 301',
+    },
+];
+export const scheduleData = [
+    {
+        id: 1,
+        name: '数据结构',
+        start: 9,
+        duration: 2,
+        weekday: 2,
+    },
+    {
+        id:3,
+        name:'组成原理',
+        start:9,
+        duration: 1,
+        weekday: 1
+    },
+    {
+        id: 2,
+        name: '计算机网络',
+        start: 12,
+        duration: 1,
+        weekday: 5,
+    },
+];
+export const examData = [
+    { id: 1, name: 'Test 1', start: '2022-04-01', end: '2022-04-02', location: 'Location 1' },
+    { id: 2, name: 'Test 2', start: '2022-04-03', end: '2022-04-04', location: 'Location 2' },
+    { id: 3, name: 'Test 3', start: '2022-04-05', end: '2022-04-06', location: 'Location 3' },
+];
+export const eventData=[
+    {
+        id: 1,
+        name: 'Laundry',
+        start: '2023-04-01 10:00',
+        duration: '1 hour',
+        reType: 'none',
+        location: 'Home',
+        online: false
+    },
+    {
+        id: 2,
+        name: 'Grocery shopping',
+        start: '2023-04-03 14:00',
+        duration: '2 hours',
+        reType: 'none',
+        location: 'Supermarket',
+        online: false
+    },
+    {
+        id: 3,
+        name: 'Read book',
+        start: '2023-04-05 19:00',
+        duration: '30 minutes',
+        reType: 'daily',
+        location: 'Home',
+        online: true
+    },
+    {
+        id: 4,
+        name: 'Go for a run',
+        start: '2023-04-07 08:00',
+        duration: '1 hour',
+        reType: 'weekly',
+        location: 'Park',
+        online: false
+    },
+    {
+        id: 5,
+        name: 'Clean bathroom',
+        start: '2023-04-10 10:00',
+        duration: '1 hour',
+        reType: 'none',
+        location: 'Home',
+        online: false
+    },
+    {
+        id: 6,
+        name: 'Attend meeting',
+        start: '2023-04-12 15:00',
+        duration: '1 hour',
+        reType: 'monthly',
+        location: 'Online',
+        online: true
+    }
+];
